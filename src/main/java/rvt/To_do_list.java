fbec6472dbb49442437415a57e53c9669bf2ed5b
24DPTKasa <A240746TK@RVT.lv>
2026-01-13 10:49:45 +0200

    Todolist

---
+++ src/main/java/rvt/To_do_list.java
@@ -0,0 +1,33 @@
+package rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class To_do_list {
+    public static void main(String[] args) {
+        
+    }
+public class TodoList
+    {
+        Scanner scanner = new Scanner(System.in);
+        TodoList list = new TodoList();
+        
+
+        public void add(String task)
+        {
+            System.out.println();
+            list.add(scanner.nextLine());  
+        }
+        public void print()
+        {
+
+        }
+        public void remove(int number)
+        {
+
+        }    
+    
+    }
+    
+    
+}
