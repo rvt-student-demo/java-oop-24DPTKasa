636d37620d91d018aedbaaab8c4808539207f262
24DPTKasa <A240746TK@RVT.lv>
2026-01-20 10:49:41 +0200

    todo uzd

---
+++ src/main/java/rvt/TodoList.java
@@ -0,0 +1,35 @@
+package rvt;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TodoList {
+        ArrayList<String> todolist = new ArrayList<>();
+        private final String filePath = "C:\\Users\\A240746TK\\Downloads\\Java\\java-oop-24DPTKasa\\data\\todo.csv";
+
+        public void add(String task)
+        {
+            todolist.add(task);
+            
+        }
+    
+
+        public void print()
+        {
+            for(int i = 0; todolist.size() > i; i++) {
+                System.out.println((i+1) + ":" + todolist);
+            }
+        }
+        
+
+
+        public void remove(int number)
+        {
+
+        }    
+    
+    
+    
+    
+}
---
+++ src/main/java/rvt/Todolist_test.java
@@ -0,0 +1,11 @@
+package rvt;
+import java.util.Scanner;
+
+public class Todolist_test {
+    public static void main(String[] args) {
+        TodoList list = new TodoList();{
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println();
+        }
+    }
+}
