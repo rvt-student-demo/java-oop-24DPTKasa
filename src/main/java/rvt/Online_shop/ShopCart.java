147b2f41be92c955e65538154cffeaaa0eaf2197
Timurs Kasatkins <timyrkas12@gmail.com>
2026-06-03 15:55:36 +0300

    Online Shop - part 5, 6, 7 finished

--- src/main/java/rvt/Online_shop/ShopCart.java
+++
@@ -1,9 +0,0 @@
-package rvt.Online_shop;
-
-public class ShopCart{
-    private Map<Product, Item> items;
-
-    public void add(String product, int price){
-        
-    }
-}
--- src/main/java/rvt/Online_shop/ShoppingCart.java
+++ src/main/java/rvt/Online_shop/ShoppingCart.java
@@ -1,10 +1,46 @@
 package rvt.Online_shop;
 
-public class ShoppingCart {
-    public void add(String product, int price){
+import java.util.HashMap;
+import java.util.Map;
 
+public class ShoppingCart {
+    private Map<String, Item> items;
+
+    public ShoppingCart() {
+        this.items = new HashMap<>();
     }
-    public int price(){
-        return price();
+
+    public void add(String product, int price) {
+        if (this.items.containsKey(product)) {
+            this.items.get(product).increaseQuantity();
+        } else {
+            this.items.put(product, new Item(product, 1, price));
+        }
     }
+
+    public int price() {
+        int totalPrice =0;
+        for (Item item : this.items.values()) {
+            totalPrice += item.price();
+        }
+        return totalPrice;
+    }
+
+    public void print() {
+        for (Item item : this.items.values()) {
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        cart.add("Milk", 3);
+        cart.add("buttermilk", 2);
+        cart.add("chocolate", 10);
+
+        System.out.println("Items in your cart:");
+        cart.print();
+        System.out.println("Total price: " + cart.price());
+    }
+
 }
